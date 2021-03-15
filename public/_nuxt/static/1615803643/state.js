faf9c18facdb2444248ceb991d0c8cd399e60f25
gobltpqls99 <gobltpqls99@naver.com>
2021-03-15 19:26:52 +0900

    ..

---
+++ public/_nuxt/static/1615803643/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615803643",layout:"layout-default",error:a,state:{list:[],view:{}},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
